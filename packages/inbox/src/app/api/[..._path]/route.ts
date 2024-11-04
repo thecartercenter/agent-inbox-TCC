11d2bb6c2e278f7a7f877a078186788f44b93918
bracesproul <braceasproul@gmail.com>
2024-11-04 15:41:35 -0800

    init commit agent inbox

---
+++ packages/inbox/src/app/api/[..._path]/route.ts
@@ -0,0 +1,86 @@
+import { LANGGRAPH_API_URL } from "../../../constants";
+import { NextRequest, NextResponse } from "next/server";
+
+function getCorsHeaders() {
+  return {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "*",
+  };
+}
+
+async function handleRequest(req: NextRequest, method: string) {
+  try {
+    const path = req.nextUrl.pathname.replace(/^\/?api\//, "");
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.search);
+    searchParams.delete("_path");
+    searchParams.delete("nxtP_path");
+    const queryString = searchParams.toString()
+      ? `?${searchParams.toString()}`
+      : "";
+
+    const options: RequestInit = {
+      method,
+      headers: {
+        "x-api-key": process.env.LANGCHAIN_API_KEY || "",
+      },
+    };
+
+    if (["POST", "PUT", "PATCH"].includes(method)) {
+      options.body = await req.text();
+    }
+
+    const res = await fetch(
+      `${LANGGRAPH_API_URL}/${path}${queryString}`,
+      options
+    );
+
+    if (res.status >= 400) {
+      console.error("ERROR IN PROXY", res.status, res.statusText);
+      return new Response(res.body, {
+        status: res.status,
+        statusText: res.statusText,
+      });
+    }
+
+    const headers = new Headers({
+      ...getCorsHeaders(),
+    });
+    // Safely add headers from the original response
+    res.headers.forEach((value, key) => {
+      try {
+        headers.set(key, value);
+      } catch (error) {
+        console.warn(`Failed to set header: ${key}`, error);
+      }
+    });
+
+    return new Response(res.body, {
+      status: res.status,
+      statusText: res.statusText,
+      headers,
+    });
+  } catch (e: any) {
+    console.error("Error in proxy");
+    console.error(e);
+    console.error("\n\n\nEND ERROR\n\n");
+    return NextResponse.json({ error: e.message }, { status: e.status ?? 500 });
+  }
+}
+
+export const GET = (req: NextRequest) => handleRequest(req, "GET");
+export const POST = (req: NextRequest) => handleRequest(req, "POST");
+export const PUT = (req: NextRequest) => handleRequest(req, "PUT");
+export const PATCH = (req: NextRequest) => handleRequest(req, "PATCH");
+export const DELETE = (req: NextRequest) => handleRequest(req, "DELETE");
+
+// Add a new OPTIONS handler
+export const OPTIONS = () => {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      ...getCorsHeaders(),
+    },
+  });
+};
---
+++ packages/inbox/src/constants.ts
@@ -0,0 +1,2 @@
+export const LANGGRAPH_API_URL =
+  process.env.LANGGRAPH_API_URL ?? "http://localhost:53404";
