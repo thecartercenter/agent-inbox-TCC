d446d157e2e70dd8bde159eaf0fe704c1204695a
bracesproul <braceasproul@gmail.com>
2024-12-11 14:22:29 -0800

    fix: Drop passthrough api route

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -7,13 +7,11 @@ export const createClient = ({
   deploymentUrl: string;
   langchainApiKey: string;
 }) => {
-  // const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api";
   return new Client({
     apiUrl: deploymentUrl,
     defaultHeaders: {
       "x-api-key": langchainApiKey,
-      // "DEPLOYMENT-URL": deploymentUrl,
-      // "LANGCHAIN-API-KEY": langchainApiKey,
     },
   });
 };
+2
